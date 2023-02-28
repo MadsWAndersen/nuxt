8acb7e8666c9896b4300a4e8c57bbf1b07522c95
Jacob Welander <Jacobwelander1994@gmail.com>
2023-02-28 12:14:06 +0100

    adds dynamic subpages

--- app.vue
+++ app.vue
@@ -20,6 +20,10 @@
     {
       "path": "/blog",
       "component": "pages/index.vue"
+    },
+    {
+      "path": "/blog/[id]artical.vue",
+      "component": "pages/[id].vue"
     }
   ]
 
@@ -32,6 +36,8 @@
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+        <li><NuxtLink to="/blog/id">id</NuxtLink></li>
+        <li><NuxtLink to="/blog/test">test</NuxtLink></li>
         <li><NuxtLink to="/contact">Contact</NuxtLink></li>
         <li><NuxtLink to="/about/history">History</NuxtLink></li>
       </ul>
---
+++ pages/blog/[id]artical.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const id = useRoute().params;
+console.log(id);
+</script>
+
+<template>
+    <div>
+        this is the {{ id.idartical }} subpage
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
